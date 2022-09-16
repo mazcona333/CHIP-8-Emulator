7b57dcf8eeb788c3b375cfd89f290ceab32c7841
mazcona333 <mazcona333@gmail.com>
2022-09-16 20:50:23 +0200

    Declaration of OPs and implementation of 14 firsts

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -8,35 +8,35 @@ const unsigned int FONTSET_START_ADDRESS = 0x50;
 const unsigned int FONTSET_SIZE = 80;
 
 uint8_t fontset[FONTSET_SIZE] =
-{
-	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-	0x20, 0x60, 0x20, 0x20, 0x70, // 1
-	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
 Chip8::Chip8()
-    {
-    //Initialize Program Counter to START_ADDRESS
+{
+    // Initialize Program Counter to START_ADDRESS
     pc = START_ADRESS;
 
     // Load fonts into memory
-	for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
-	{
-		memory[FONTSET_START_ADDRESS + i] = fontset[i];
-	} 
+    for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
+    {
+        memory[FONTSET_START_ADDRESS + i] = fontset[i];
+    }
 }
 
 bool Chip8::LoadROM(char const *filename)
@@ -45,17 +45,129 @@ bool Chip8::LoadROM(char const *filename)
 
     if (file.is_open())
     {
-        //Load ROM in memory, starting from START_ADDRESS
+        // Load ROM in memory, starting from START_ADDRESS
         char c;
         for (long i = 0; file.get(c); i++)
         {
-            if((START_ADRESS + i) > MAX_MEMORY){
+            if ((START_ADRESS + i) > MAX_MEMORY)
+            {
                 return false;
             }
             memory[START_ADRESS + i] = c;
         }
         return true;
-    }else{
+    }
+    else
+    {
         return false;
     }
+}
+
+void Chip8::OP_NULL() {}
+
+void Chip8::OP_00E0()
+{
+    memset(video, 0, sizeof(video));
+}
+
+void Chip8::OP_00EE()
+{
+    sp--;
+    pc = stack[sp];
+}
+
+void Chip8::OP_1nnn()
+{
+    uint16_t address = opcode & 0x0FFFu;
+
+    pc = address;
+}
+
+void Chip8::OP_2nnn()
+{
+    uint16_t address = opcode & 0x0FFFu;
+
+    stack[sp] = pc;
+    sp++;
+    pc = address;
+}
+
+void Chip8::OP_3xkk()
+{
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    if (registers[Vx] == byte)
+    {
+        pc += 2;
+    }
+}
+
+void Chip8::OP_4xkk()
+{
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    if (registers[Vx] != byte)
+    {
+        pc += 2;
+    }
+}
+
+void Chip8::OP_5xy0()
+{
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vx] == registers[Vx])
+    {
+        pc += 2;
+    }
+}
+
+void Chip8::OP_6xkk()
+{
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    registers[Vx] = byte;
+}
+
+void Chip8::OP_7xkk()
+{
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    registers[Vx] += byte;
+}
+
+void Chip8::OP_8xy0(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] = registers[Vy];
+}
+
+void Chip8::OP_8xy1()
+{
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] |= registers[Vy];
+}
+
+void Chip8::OP_8xy2()
+{
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] &= registers[Vy];
+}
+
+void Chip8::OP_8xy3()
+{
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] ^= registers[Vy];
 }
--- src/chip8.h
+++ src/chip8.h
@@ -3,23 +3,129 @@
 
 #include <cstdint>
 
-class Chip8{
-    public:
-        Chip8();
-        bool LoadROM(char const* filename);
+class Chip8
+{
+public:
+    Chip8();
+    bool LoadROM(char const *filename);
 
-    private:
-        uint8_t registers[16];
-        uint8_t memory[4096];
-        uint16_t index;
-        uint16_t pc;
-        uint16_t stack[16];
-        uint8_t sp;
-        uint8_t delayTimer;
-        uint8_t soundTimer;
-        uint8_t keypad[16];
-        uint32_t video [64 * 32];
-        uint16_t opcode;
+private:
+    uint8_t registers[16];
+    uint8_t memory[4096];
+    uint16_t index;
+    uint16_t pc;
+    uint16_t stack[16];
+    uint8_t sp;
+    uint8_t delayTimer;
+    uint8_t soundTimer;
+    uint8_t keypad[16];
+    uint32_t video[64 * 32];
+    uint16_t opcode;
+
+    // Do nothing
+    void OP_NULL();
+
+    // CLS
+    void OP_00E0(); // Clear display
+
+    // RET
+    void OP_00EE(); // Return from subroutine
+
+    // JP address
+    void OP_1nnn(); // Jump to location nn
+
+    // CALL address
+    void OP_2nnn(); // Call subroutine at nnn
+
+    // SE Vx, byte
+    void OP_3xkk(); // Skip next instruction if Vx == kk
+
+    // SNE Vx, byte
+    void OP_4xkk(); // Skip next instruction if Vx != kk
+
+    // SE Vx, Vy
+    void OP_5xy0(); // Skip next instruction if Vx == Vy
+
+    // LD Vx, byte
+    void OP_6xkk(); // Set Vx = kk
+
+    // ADD Vx, byte
+    void OP_7xkk(); // Set Vx = Vx + kk
+
+    // LD Vx, Vy
+    void OP_8xy0(); // Set Vx = Vy
+
+    // OR Vx, Vy
+    void OP_8xy1(); // Set Vx = Vx OR Vy
+
+    // AND Vx, Vy
+    void OP_8xy2(); // Set Vx = Vx AND Vy
+
+    // XOR Vx, Vy
+    void OP_8xy3(); // Set Vx = Vx XOR Vy
+
+    // ADD Vx, Vy
+    void OP_8xy4(); // Set Vx = Vx + Vy, set VF = carry. If Vx + Vy > 255, VF = 1, otherwise 0. Only the lowest 8 bits stored in Vx.
+
+    // SUB Vx, Vy
+    void OP_8xy5(); // Set Vx = Vx - Vy, set VF = NOT borrow. If Vx > Vy, then VF is set to 1, otherwise 0.
+
+    // SHR Vx
+    void OP_8xy6(); // If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
+
+    // SUBN Vx, Vy
+    void OP_8xy7(); // Set Vx = Vy - Vx, set VF = NOT borrow. If Vy > Vx, then VF is set to 1, otherwise 0.
+
+    // SHL Vx
+    void OP_8xyE(); // Set Vx = Vx SHL 1. If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
+
+    // SNE Vx, Vy
+    void OP_9xy0(); // Skip next instruction if Vx != Vy.
+
+    // LD I, address
+    void OP_Annn(); // Set I = nnn.
+
+    // JP V0, address
+    void OP_Bnnn(); // Jump to location nnn + V0.
+
+    // RND Vx, byte
+    void OP_Cxkk(); // Set Vx = random byte AND kk.
+
+    // DRW Vx, Vy, height
+    void OP_Dxyn(); // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+
+    // SKP Vx
+    void OP_Ex9E(); // Skip next instruction if key with the value of Vx is pressed.
+
+    // SKNP Vx
+    void OP_ExA1(); // Skip next instruction if key with the value of Vx is not pressed.
+
+    // LD Vx, DT
+    void OP_Fx07(); // Set Vx = delay timer value.
+
+    // LD Vx, K
+    void OP_Fx0A(); // Wait for a key press, store the value of the key in Vx.
+
+    // LD DT, Vx
+    void OP_Fx15(); // Set delay timer = Vx.
+
+    // LD ST, Vx
+    void OP_Fx18(); // Set sound timer = Vx.
+
+    // ADD I, Vx
+    void OP_Fx1E(); // Set I = I + Vx.
+
+    // LD F, Vx
+    void OP_Fx29(); // Set I = location of sprite for digit Vx.
+
+    // LD B, Vx
+    void OP_Fx33(); // Store BCD representation of Vx in memory locations I, I+1, and I+2. The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.
+
+    // LD [I], Vx
+    void OP_Fx55(); // Store registers V0 through Vx in memory starting at location I.
+
+    // LD Vx, [I]
+    void OP_Fx65(); // Read registers V0 through Vx from memory starting at location I.
 };
 
-#endif //CHIP8_H
+#endif // CHIP8_H
